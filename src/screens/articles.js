bf3dcacfbd1bd6ab16c6fad902159d2f264ec4da
Andi Surya <andi502.as@gmail.com>
2024-06-19 08:51:22 +0700

    (fix): fix import

--- src/screens/articles.js
+++ src/screens/articles.js
@@ -1,10 +1,11 @@
+/* eslint-disable max-len */
 import RemoteData from '../data/remote-data';
 // import Utils from '../utils/utils';
-import TemplateCreator from '../scripts/views/template-creator';
+import TemplateCreator from '../scripts/view/tempalates/template-creator';
 
 const Articles = {
-    async render() {
-        return `      
+  async render() {
+    return `      
             <h2 tabindex=0 class="page-title">Artikel untuk anda</h2>
             <div class="custom-divider"></div>
             <div id="article-list-page">
@@ -12,25 +13,25 @@ const Articles = {
                 </div>
             </div>
         `;
-    },
+  },
 
-    async afterRender() {
-        // const vegetableListPage = document.querySelector('#vegetable-list-page');
-        // vegetableListPage.innerHTML += createLoadingIndicatorTemplate();
+  async afterRender() {
+    // const vegetableListPage = document.querySelector('#vegetable-list-page');
+    // vegetableListPage.innerHTML += createLoadingIndicatorTemplate();
 
-        const articleList = await RemoteData.getArticles();
+    const articleList = await RemoteData.getArticles();
 
-        console.log(`article ${articleList}`);
+    console.log(`article ${articleList}`);
 
-        // const loadingIndicator = vegetableListPage.querySelector('#loading-indicator-component');
-        // Utils.hideElement(loadingIndicator);
+    // const loadingIndicator = vegetableListPage.querySelector('#loading-indicator-component');
+    // Utils.hideElement(loadingIndicator);
 
-        // const vegetableListComponent = document.querySelector('#vegetable-list-component');
-        // vegetableList.forEach((vegetable) => {
-        //     // eslint-disable-next-line max-len
-        //     vegetableListComponent.innerHTML += TemplateCreator.createVegetableItemTemplate(vegetable);
-        // });
-    },
+    // const vegetableListComponent = document.querySelector('#vegetable-list-component');
+    // vegetableList.forEach((vegetable) => {
+    //     // eslint-disable-next-line max-len
+    //     vegetableListComponent.innerHTML += TemplateCreator.createVegetableItemTemplate(vegetable);
+    // });
+  },
 };
 
 export default Articles;
